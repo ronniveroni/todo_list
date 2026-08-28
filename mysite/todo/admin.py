67aa8d97d62339d35bd21e4c860f3ed26edd099e
Veronika <lelekaveronika1@gmail.con>
2026-08-28 14:52:45 +0300

    +Custom admin view. (data, name, status) stulpeliai

--- mysite/todo/admin.py
+++ mysite/todo/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Task,Tag
 
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['date', 'name', 'status']
+
 # Register your models here.
-admin.site.register(Task)
+admin.site.register(Task,TaskAdmin)
 admin.site.register(Tag)
--- mysite/todo/models.py
+++ mysite/todo/models.py
@@ -6,15 +6,23 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
     def __str__(self):
         return self.name
 
 class Task(models.Model):
     name = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL,null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField(Tag, blank=True)
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
 
     LOAN_STATUS = (
         ('n', 'Not started'),
@@ -25,6 +33,5 @@ class Task(models.Model):
     status = models.CharField(verbose_name="Status", max_length=1, choices=LOAN_STATUS, blank=True, default="n")
 
 
-
     def __str__(self):
-        return f'{self.name} {self.content} {self.user} {self.date}'
+        return f'{self.date.strftime("%Y-%m-%d %H:%M")}, {self.name} {self.tags}, {self.status}'

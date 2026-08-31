4a9b15f6e98f0482ca103e7031765152779d3bdb
Veronika <lelekaveronika1@gmail.con>
2026-08-31 14:31:49 +0300

    +Pridėtas visų todo užduočių atvaizdavimas.

--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from todo.models import Task
 
 # Create your views here.
 def index(request):
-    return render(request, template_name='index.html')
+    context = {
+    'tasks': Task.objects.all(),
+    'num_tasks_not_started': Task.objects.filter(status='n').count(),
+    }
+    return render(request, template_name='index.html', context=context)
